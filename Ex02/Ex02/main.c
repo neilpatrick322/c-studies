fd36a4212784c2516a13aa549bd07c3e47b75bfb
neilpatrick322 <cabralpatrick322@gmail.com>
2021-04-21 12:42:11 +0800

    Basic refactoring.

--- Ex02/Ex02/main.c
+++ Ex02/Ex02/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int main()
-{
-    // Initialize Variable
-    double students_average = 0; // initial value is 0
-    double students_total_grade = 0; // initial value is 0
-    int student_grade[10]; // access from [0] --> [9]
 
+// Initialize Variable
+double students_average = 0; // initial value is 0
+double students_total_grade = 0; // initial value is 0
+int student_grade[10]; // access from [0] --> [9]
+
+void scan_inputs() {
     // Get Inputs
     for (int i = 0; i < 10; i++) {
-        printf("Enter student%d grade(0 to 100) and then press Enter:\n", i+1);
+        printf("Enter student%d grade(0 to 100) and then press Enter:\n", i + 1);
         scanf_s("%d", &student_grade[i]);
 
         if (student_grade[i] < 0) {
@@ -23,13 +23,22 @@ int main()
 
         students_total_grade = students_total_grade + student_grade[i];
     }
+}
 
-    // Calculate
-    students_average = students_total_grade / 10;
-
-    // Present Output;
+void print_outputs() {
     printf("Students total grade %g\n", students_total_grade);
     printf("Students total average %g\n", students_average);
+}
+
+double calculate_average() {
+    return (double)(students_total_grade / 10.0);
+}
+
+int main()
+{
+    scan_inputs();
+    students_average = calculate_average();
+    print_outputs();
   
     return 0;
 }
