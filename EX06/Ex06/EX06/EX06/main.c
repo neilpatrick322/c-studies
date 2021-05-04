b681cb72a891161d8018eef1cd80517bb2e0a012
neilpatrick322 <cabralpatrick322@gmail.com>
2021-05-04 13:54:20 +0800

    Update main.c

--- EX06/Ex06/EX06/EX06/main.c
+++ EX06/Ex06/EX06/EX06/main.c
@@ -5,10 +5,10 @@ int main()
 	int i;
 	int n;
 
-	printf("enter the no. then press Enter\n");
+	printf("Enter the no. then press Enter\n");
 	scanf_s("%d", &n);
 
-	for (i = 1; i <= 10; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		printf(" %d * %d = %d \n", n, i,(n * i));
 	}
