94f0f995cc53e15d571e8c282ef5742ea8f6fc94
neilpatrick322 <cabralpatrick322@gmail.com>
2021-05-14 15:21:18 +0800

    EX13
    structure

--- ex13/EX13/EX13/main.c
+++ ex13/EX13/EX13/main.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct student {
-    char name[20];
-    int age[10];
-    int r_no[10];
+// 8 bits = 1 byte
 
-
+struct student  { // total in bytes = 28
+    char name[20]; // 20 bytes (1 char is 1 byte)
+    int age; 
+    int r_no;
 };
 
     int main() {
-        int max = 10;
+        int max =10;
         int i;
-        struct student s[10];
+        struct student s[10] ; // 2000dec -> 2280dec
 
 
         for (i = 0; i < max; i++) {
-            char name[20];
-            int age[10];
-            int r_no[10];
-
             printf("Enter name of the student:");
-            scanf_s("%s", name, (unsigned)_countof(name));
+            scanf_s("%s", s[i].name, (unsigned)_countof(s[i].name));
             printf("Enter age of the student:");
-            scanf_s("%d", age, ((unsigned)_countof(age)));
-            printf("Enter student's roll no :");
-            scanf_s("%d", r_no, ((unsigned)_countof(r_no)));
-
+            scanf_s("%d", &(s[i].age));
+            printf("Enter student's roll no :"); 
+            scanf_s("%d", &(s[i].r_no));
         }
-        for (int i = 0; i < max; i++)
+        for ( i = 0; i < max; i++)
         {
-            printf("Name : %s", s[i].name);
-            printf("age : %d", s[i].age);
-            printf("roll no. : %d", s[i].r_no);
+            printf("Name : %s | Age : %d | Roll no : %d ", s[i].name, s[i].age, s[i].r_no);
             printf("\n");
         }
 
