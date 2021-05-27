526215db3b0effa47e23ebe68d63022fc88778c9
neilpatrick322 <cabralpatrick322@gmail.com>
2021-05-27 10:59:16 +0800

    EX21
    base

--- EX21/EX21/EX21/main.c
+++ EX21/EX21/EX21/main.c
@@ -1,46 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-int main()
+// Measure-Command { ./EX21.exe 99999 } # 1418.1111 
+
+char buf[80];
+FILE* fp = NULL;
+long i, prime, lim_up, n;
+
+int main(int argc, char* argv[], char* environment[])
 {
-    FILE* fp;
+	errno_t error = fopen_s(&fp, "prime_numbers.txt", "w");
+	if (fp == NULL)
+	{
+		printf("Error!");
+		exit(1);
+	}
+	printf("\nUpper Limit: %s", argv[1]);
+	char* eptr;
 
-    int i, prime, lim_up, n;
-   
-
-
-    fp = fopen("prime numbers.txt", "w");
-    if (fp == NULL)
-    {
-        printf("Error!");
-        exit(1);
-    }
-
-    printf("\nEnter number : ");
-    scanf_s("%d", &lim_up);
+	lim_up = strtol(argv[1], &eptr, 10);
 
 
-    for (n = 1; n < lim_up; n++)
-    {
-        prime = 1;
-        for (i = 2; i < n; i++)
-            if (n % i == 0)
-            {
-                prime = 0;
-                break;
-            }
-        if (prime)
-        {
-            printf(" %d ", n);
+	for (n = 1; n < lim_up; n++)
+	{
 
-            fprintf(fp, " prime numbers : %d \n ", n);
-        }
-    }
-    printf("\n Details successfully written to the file\n\n");
+		prime = 1;
+		for (i = 2; i < n; i++)
+			if (n % i == 0)
+			{
+				prime = 0;
+				break;
+			}
+		if (prime)
+		{
+			//printf(" %d ", n);
+			fprintf(fp, "prime numbers : ");
+			fprintf(fp, " %d\n ", n);
+		}
 
+	}
+	fclose(fp);
 
-    fclose(fp);
-   
+	printf("\n Details successfully written to the file\n\n");
 
-    return 0;
+	return 0;
 }
